92c0fac8e5e1462e2d4aea843688bcac6a437f1a
edhubbard1994 <edhubbard1994@gmail.com>
2021-11-13 15:12:09 -0600

    code styling

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <omp.h>
+#include <math.h>
+#include <algorithm>
 
 
 
@@ -8,6 +10,20 @@
 
 using std::cin;
 using std::cout;
+
+
+
+template<typename T>
+std::vector<T> getData() {
+    std::vector<T> data;
+    data.resize(PROBLEM_SIZE);
+    srand(time(NULL));
+	for (auto index = data.begin(); index != data.end(); ++index) {
+		*index = (rand() % highest) + lowest;
+	}
+}
+
+
 
 template<typename T>
 void kmeans(std::vector<T> data,T m1, T m2) {
@@ -22,8 +38,7 @@ void kmeans(std::vector<T> data,T m1, T m2) {
       
     T om1,om2;    //old means
  
-    do
-    {
+    do {
      
     //saving old means
     om1=m1;
@@ -31,8 +46,7 @@ void kmeans(std::vector<T> data,T m1, T m2) {
  
     //creating clusters
     i1=i2=i3=0;
-    for(i1=0;i1<size;i1++)
-    {
+    for(i1=0;i1<size;i1++) {
         //calculating distance to means
         t1=k0[i1]-m1;
         if(t1<0){t1=-t1;}
@@ -40,14 +54,12 @@ void kmeans(std::vector<T> data,T m1, T m2) {
         t2=k0[i1]-m2;
         if(t2<0){t2=-t2;}
  
-        if(t1<t2)
-        {
+        if(t1<t2) {
             //near to first mean
             k1[i2]=k0[i1];
             i2++;
         }
-        else
-        {
+        else {
             //near to second mean
             k2[i3]=k0[i1];
             i3++;
@@ -57,36 +69,33 @@ void kmeans(std::vector<T> data,T m1, T m2) {
  
     t2=0;
     //calculating new mean
-    for(t1=0;t1<i2;t1++)
-    {
+    for(t1=0;t1<i2;t1++) {
         t2=t2+k1[t1];
     }
     m1=t2/i2;
  
     t2=0;
-    for(t1=0;t1<i3;t1++)
-    {
+    for(t1=0;t1<i3;t1++) {
         t2=t2+k2[t1];
     }
     m2=t2/i3;
  
     //printing clusters
     cout<<"\nCluster 1:";
-    for(t1=0;t1<i2;t1++)
-    {
+    for(t1=0;t1<i2;t1++) {
         cout<<k1[t1]<<" ";
     }
     cout<<"\nm1="<<m1;
  
     cout<<"\nCluster 2:";
-    for(t1=0;t1<i3;t1++)
-    {
+    for(t1=0;t1<i3;t1++) {
         cout<<k2[t1]<<" ";
     }
     cout<<"\nm2="<<m2;
  
     cout<<"\n ----";
-    }while(m1!=om1&&m2!=om2);
+    
+    } while(m1!=om1&&m2!=om2);
  
     cout<<"\n Clusters created";
  
